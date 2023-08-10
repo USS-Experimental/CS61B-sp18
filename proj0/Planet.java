5cfae57a5fe94063639c0b08afc924b85928506f
USS-Experimental <liyuting2004@gmail.com>
2023-08-10 12:58:26 +0800

    complete Description of Planet.java

--- proj0/Planet.java
+++ proj0/Planet.java
@@ -1,14 +1,47 @@
+
+/**
+ *  Definition of Planet Class and its methods.
+ */
 public class Planet {
 
+    /**
+     * Define the gravitational constant.
+     */
     public static double gravitationalConstant = 6.67e-11;
 
+    /**
+     * The current x position of the Planet.
+     */
     public double xxPos;
+
+    /**
+     * The current y position of the Planet.
+     */
     public double yyPos;
+
+    /**
+     * The current velocity in the x direction of the Planet.
+     */
     public double xxVel;
+
+    /**
+     * The current velocity in the y direction of the Planet.
+     */
     public double yyVel;
+
+    /**
+     * The mass of the Planet.
+     */
     public double mass;
+
+    /**
+     * The name of the file that corresponds to the image that depicts the Planet.
+     */
     public String imgFileName;
 
+    /**
+     * The constructor of the Planet class.
+     */
     public Planet(double xP, double yP, double xV,
               double yV, double m, String img){
         xxPos = xP;
@@ -19,6 +52,9 @@ public class Planet {
         imgFileName = img;
     }
 
+    /**
+     * Another constructor of the Planet class, take in a Planet object.
+     */
     public Planet(Planet p) {
         xxPos = p.xxPos;
         yyPos = p.yyPos;
@@ -28,26 +64,41 @@ public class Planet {
         imgFileName = p.imgFileName;
     }
 
+    /**
+     * Calculates the distance between two Planets.
+     */
     public double calcDistance(Planet p) {
         return Math.sqrt((this.xxPos - p.xxPos) * (this.xxPos - p.xxPos) 
             + (this.yyPos - p.yyPos) * (this.yyPos - p.yyPos));
     }
 
+    /**
+     * Calculates the force between two Planets.
+     */
     public double calcForceExertedBy(Planet p) {
         return gravitationalConstant * this.mass * p.mass 
             / (this.calcDistance(p) * this.calcDistance(p));
     }
 
+    /**
+     * Calculates the force in the x direction between two Planets.
+     */
     public double calcForceExertedByX(Planet p) {
         return this.calcForceExertedBy(p) * (p.xxPos - this.xxPos)
             / this.calcDistance(p);
     }
 
+    /**
+     * Calculates the force in the y direction between two Planets.
+     */
     public double calcForceExertedByY(Planet p) {
         return this.calcForceExertedBy(p) * (p.yyPos - this.yyPos)
             / this.calcDistance(p);
     }
 
+    /**
+     * Calculates the total force in the x direction of a Planet.
+     */
     public double calcNetForceExertedByX(Planet[] p) {
         double sum = 0;
         for (Planet s : p) {
@@ -59,6 +110,9 @@ public class Planet {
         return sum;
     }
 
+    /**
+     * Calculates the total force in the y direction of a Planet.
+     */
     public double calcNetForceExertedByY(Planet[] p) {
         double sum = 0;
         for (Planet s : p) {
@@ -70,6 +124,9 @@ public class Planet {
         return sum;
     }
 
+    /**
+     * Update the status of a Planet.
+     */
     public void update(double dt, double fX, double fY) {
         this.xxVel += fX / this.mass * dt;
         this.yyVel += fY / this.mass * dt;
